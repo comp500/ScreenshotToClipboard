5404ed1e79af6ffb3ee83f3dfa5a0b6808eb38fc
comp500 <comp500@users.noreply.github.com>
2019-07-10 19:31:11 +0100

    Add actual code

--- src/main/java/link/infra/screenshotclipboard/ScreenshotToClipboard.java
+++ src/main/java/link/infra/screenshotclipboard/ScreenshotToClipboard.java
@@ -1,29 +1,51 @@
 package link.infra.screenshotclipboard;
 
-import net.minecraft.init.Blocks;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import net.minecraftforge.client.event.ScreenshotEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import org.apache.logging.log4j.Logger;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod(modid = ScreenshotToClipboard.MODID, name = ScreenshotToClipboard.NAME, version = ScreenshotToClipboard.VERSION)
+@Mod.EventBusSubscriber
 public class ScreenshotToClipboard {
 	public static final String MODID = "screenshotclipboard";
 	public static final String NAME = "Screenshot to Clipboard";
 	public static final String VERSION = "1.12.2-1.0.0";
 
-	private static Logger logger;
-
-	@EventHandler
-	public void preInit(FMLPreInitializationEvent event) {
-		logger = event.getModLog();
+	@SubscribeEvent
+	public static void handleScreenshot(ScreenshotEvent event) {
+		Transferable trans = getTransferableImage(event.getImage());
+		Clipboard c = Toolkit.getDefaultToolkit().getSystemClipboard();
+		c.setContents(trans, null);
 	}
 
-	@EventHandler
-	public void init(FMLInitializationEvent event)
-	{
-		// some example code
-		logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+	private static Transferable getTransferableImage(final BufferedImage bufferedImage) {
+		return new Transferable() {
+			@Override
+			public DataFlavor[] getTransferDataFlavors() {
+				return new DataFlavor[] { DataFlavor.imageFlavor };
+			}
+
+			@Override
+			public boolean isDataFlavorSupported(DataFlavor flavor) {
+				return DataFlavor.imageFlavor.equals(flavor);
+			}
+
+			@Override
+			public Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException, IOException {
+				if (DataFlavor.imageFlavor.equals(flavor)) {
+					return bufferedImage;
+				}
+				return null;
+			}
+		};
 	}
+
 }
