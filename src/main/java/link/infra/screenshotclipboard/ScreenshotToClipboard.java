fdbdc2e2aa2536de09d87db2bac02776b1c549c0
comp500 <comp500@users.noreply.github.com>
2019-07-11 17:24:40 +0100

    Improve performance

--- src/main/java/link/infra/screenshotclipboard/ScreenshotToClipboard.java
+++ src/main/java/link/infra/screenshotclipboard/ScreenshotToClipboard.java
@@ -17,13 +17,15 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class ScreenshotToClipboard {
 	public static final String MODID = "screenshotclipboard";
 	public static final String NAME = "Screenshot to Clipboard";
-	public static final String VERSION = "1.12.2-1.0.0";
+	public static final String VERSION = "1.12.2-1.0.1";
 
 	@SubscribeEvent
 	public static void handleScreenshot(ScreenshotEvent event) {
-		Transferable trans = getTransferableImage(event.getImage());
-		Clipboard c = Toolkit.getDefaultToolkit().getSystemClipboard();
-		c.setContents(trans, null);
+		new Thread(() -> {
+			Transferable trans = getTransferableImage(event.getImage());
+			Clipboard c = Toolkit.getDefaultToolkit().getSystemClipboard();
+			c.setContents(trans, null);
+		}).start();
 	}
 
 	private static Transferable getTransferableImage(final BufferedImage bufferedImage) {
@@ -43,7 +45,7 @@ public class ScreenshotToClipboard {
 				if (DataFlavor.imageFlavor.equals(flavor)) {
 					return bufferedImage;
 				}
-				return null;
+				throw new UnsupportedFlavorException(flavor);
 			}
 		};
 	}
