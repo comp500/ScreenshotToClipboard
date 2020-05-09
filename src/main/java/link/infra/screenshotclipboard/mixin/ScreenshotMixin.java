67c306df2174db58f472a0c5834fde5fd45dbc86
comp500 <comp500@users.noreply.github.com>
2020-05-09 06:01:50 +0100

    Add support for Optifabric/Optifine (fixes #6)

--- src/main/java/link/infra/screenshotclipboard/mixin/ScreenshotMixin.java
+++ src/main/java/link/infra/screenshotclipboard/mixin/ScreenshotMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.client.util.ScreenshotUtils;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Group;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
@@ -19,13 +20,29 @@ public class ScreenshotMixin {
 
 	// Inject before it starts saving the file (HEAD is the safest place to do this)
 	@Inject(at = @At(value = "HEAD"), method = "method_1661")
+	@Group(max = 1, min = 1, name = "screenshotclipboard-screenshotCaptured")
 	private static void screenshotCaptured(NativeImage nativeImage_1, File file_1, Consumer<Text> consumer_1, CallbackInfo ci) {
 		ScreenshotToClipboard.handleScreenshot(nativeImage_1);
 	}
 
 	// Inject after saving the image, before accepting the consumer
 	@Inject(at = @At(value = "INVOKE", target = "Ljava/util/function/Consumer;accept(Ljava/lang/Object;)V", ordinal = 0, remap = false), method = "method_1661")
+	@Group(max = 1, min = 1, name = "screenshotclipboard-screenshotCapturedMac")
 	private static void screenshotCapturedMac(NativeImage nativeImage_1, File file_1, Consumer<Text> consumer_1, CallbackInfo ci) {
+		MacOSCompat.doCopyMacOS(file_1.getAbsolutePath());
+	}
+
+	// Inject before it starts saving the file (HEAD is the safest place to do this)
+	@Inject(at = @At(value = "HEAD"), method = "lambda$saveScreenshotRaw$2")
+	@Group(max = 1, min = 1, name = "screenshotclipboard-screenshotCaptured")
+	private static void screenshotCapturedOptifine(NativeImage nativeImage_1, File file_1, Object o, Consumer<Text> consumer_1, CallbackInfo ci) {
+		ScreenshotToClipboard.handleScreenshot(nativeImage_1);
+	}
+
+	// Inject after saving the image, before accepting the consumer
+	@Inject(at = @At(value = "INVOKE", target = "Ljava/util/function/Consumer;accept(Ljava/lang/Object;)V", ordinal = 1, remap = false), method = "lambda$saveScreenshotRaw$2")
+	@Group(max = 1, min = 1, name = "screenshotclipboard-screenshotCapturedMac")
+	private static void screenshotCapturedMacOptifine(NativeImage nativeImage_1, File file_1, Object o, Consumer<Text> consumer_1, CallbackInfo ci) {
 		MacOSCompat.doCopyMacOS(file_1.getAbsolutePath());
 	}
 }
