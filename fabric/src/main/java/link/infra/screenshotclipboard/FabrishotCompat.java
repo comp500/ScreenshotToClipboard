7480e7955beb4657b62432205cefabfd1950aedc
comp500 <comp500@users.noreply.github.com>
2022-02-10 14:16:21 +0000

    Add support for Fabrishot 1.6.2

--- fabric/src/main/java/link/infra/screenshotclipboard/FabrishotCompat.java
+++ fabric/src/main/java/link/infra/screenshotclipboard/FabrishotCompat.java
@@ -8,7 +8,7 @@ import me.ramidzkh.fabrishot.event.ScreenshotSaveCallback;
 public class FabrishotCompat {
 	public static void init() {
 		FramebufferCaptureCallback.EVENT.register((dim, buffer) ->
-			ScreenshotToClipboard.handleScreenshotAWT(buffer, dim.width, dim.height, 3));
+			ScreenshotToClipboard.handleScreenshotAWT(buffer, dim.width(), dim.height(), 3));
 
 		ScreenshotSaveCallback.EVENT.register(file ->
 			MacOSCompat.doCopyMacOS(file.toString()));
