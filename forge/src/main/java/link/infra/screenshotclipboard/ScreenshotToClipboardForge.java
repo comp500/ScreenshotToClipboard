a99c019f109a1b144b1fe69a5330f582ae8a08da
comp500 <comp500@users.noreply.github.com>
2022-02-10 14:05:04 +0000

    Port to 1.18

--- forge/src/main/java/link/infra/screenshotclipboard/ScreenshotToClipboardForge.java
+++ forge/src/main/java/link/infra/screenshotclipboard/ScreenshotToClipboardForge.java
@@ -5,13 +5,13 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fmllegacy.network.FMLNetworkConstants;
 import net.minecraftforge.forgespi.Environment;
+import net.minecraftforge.network.NetworkConstants;
 
 @Mod(ScreenshotToClipboard.MOD_ID)
 public class ScreenshotToClipboardForge {
 	public ScreenshotToClipboardForge() {
-		ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+		ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
 		if (Environment.get().getDist() == Dist.CLIENT) {
 			ScreenshotToClipboardForgeClient.init();
 		}
